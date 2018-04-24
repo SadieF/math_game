70f7a4f8c1a3374c6f38170057fe204b325c4e66
Sadie <sadiefreeman@gmail.com>
2018-04-24 18:11:08 +0000

    Added player names and turn swaps

--- game.rb
+++ game.rb
@@ -2,31 +2,47 @@ class Game
   attr_reader :name
 
   def initialize(name)
-    @player1 = Player.new("Player 1")
-    @player2 = Player.new("Player 2")
+    random_name1 = ['Gabe', 'Matt', 'Wendy', 'Sadie', 'Anhad', 'Alex'].sample
+    random_name2 = ['Morgan', 'Andy', 'Natalie', 'Sam', 'Dominic'].sample
+
+    @player1 = Player.new(random_name1)
+    @player2 = Player.new(random_name2)
     @current_player = @player1
+  end
+
+  def new_turn
+    if (@current_player == @player1)
+      @current_player = @player2
+    else
+      @current_player = @player1
+    end
+    puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
+    puts "<--- NEW TURN --->"
   end
 
   def play_game
     question = Question.new
-    question.ask_question
+    question.ask_question(@current_player.name)
     user_answer = $stdin.gets.chomp
     if question.verify_answer(user_answer)
-      puts "Yay! You are a genius"
+      puts "Yay #{@current_player.name}! You are a genius"
     else
-      puts "What is wrong with you. You lose a life"
+      puts "#{@current_player.name}, what is wrong with you. You lose a life"
       @current_player.lives -= 1
     end
+    new_turn
   end
 
   def start
     while @player1.is_alive and @player2.is_alive
       play_game
     end
+    puts "Well you pretty much suck. #{@current_player.name} wins with a score of #{@current_player.lives}/3!"
+    puts "<--- GAME OVER --->"
   end
+
 end
 
-new_game = Game.new(2)
-p new_game
+
 
 
--- questions.rb
+++ questions.rb
@@ -1,19 +1,19 @@
 class Question
 
-  attr_reader :num1, :num2, :name
+  attr_reader :num1, :num2
 
   def initialize
     @num1 = rand(1..20)
     @num2 = rand(1..20)
   end
 
-  def ask_question
-    puts "What does #{num1} plus #{num2} equal?"
+  def ask_question(current_player)
+    puts "#{current_player}: What does #{@num1} plus #{@num2} equal?"
     print "> "
   end
 
   def verify_answer(user_answer)
-    user_answer == "#{@num1 + num2}"
+    user_answer == "#{@num1 + @num2}"
   end
 
 end
